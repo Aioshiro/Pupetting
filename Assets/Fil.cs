886c8bb3e256540bded93e72228563b71042e6f1
Lucas Detto <lucas.detto@eleves.ec-nantes.fr>
2021-01-25 14:01:01 +0100

    Added assets and prototype scene
    Added asset for rigged skeleton and the prototype scene with hands linked to the skeleton

---
+++ Assets/Fil.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+public class Fil : MonoBehaviour
+{
+    public float maxLength;
+    public GameObject arrivee;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 distance = arrivee.transform.position - this.transform.position;
+        if (distance.magnitude < maxLength)
+        {
+            this.transform.localScale -= new Vector3(0, 0, this.transform.localScale.z);
+            this.transform.localScale += new Vector3(0, 0, 10*distance.magnitude);
+        }
+        else
+        {
+            this.transform.localScale -= new Vector3(0, 0, this.transform.localScale.z);
+            this.transform.localScale += new Vector3(0, 0, 10*maxLength);
+        }
+    }
+}
